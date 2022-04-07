67de74b1d385d8c57fac0a26d4d7cebabd453c60
Jakub Szczerba <j.szczerba@yesindeed.pl>
2022-04-07 12:35:51 +0200

    Prepare form for creating proposal.

--- src/Form/ProposalType.php
+++ src/Form/ProposalType.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace App\Form;
 
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -12,5 +16,31 @@ class ProposalType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder
+            ->add('name', TextType::class,  [
+                'label' => 'Nazwa'
+            ])
+            ->add('activity', TextType::class,  [
+                'label' => 'Aktywnosc'
+            ])
+            ->add('datetime', DateType::class,  [
+                'label' => 'Data',
+                'widget' => 'single_text',
+            ])
+            ->add('cover', FileType::class,  [
+                'label' => 'Okladka',
+                'mapped' => false,
+                'required' => false,
+            ])
+            ->add('description', TextType::class,  [
+                'label' => 'Opis'
+            ])
+            ->add('value', NumberType::class, [
+                'label' => 'Wartosc'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Zapisz',
+            ])
+        ;
     }
 }
