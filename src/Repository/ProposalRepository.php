1635071b288f258d32f1d57c296891c0b3e1aab0
Jakub Szczerba <j.szczerba@yesindeed.pl>
2022-04-08 08:25:07 +0200

    Solve problem with query for proposals

--- src/Repository/ProposalRepository.php
+++ src/Repository/ProposalRepository.php
@@ -4,10 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Proposal;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\OptimisticLockException;
-use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
-use Doctrine\ORM\Query\Expr\Join;
 
 class ProposalRepository extends ServiceEntityRepository
 {
@@ -15,7 +12,6 @@ class ProposalRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Proposal::class);
     }
-
     /**
      * @return proposals[]
      */
@@ -24,7 +20,8 @@ class ProposalRepository extends ServiceEntityRepository
         $qb = $this->createQueryBuilder('p');
 
         $qb->select('p')
-            ->where('p.id = :user')
+            ->leftJoin('p.user', 'u')
+            ->where('u.id = :user')
             ->setParameter('user', $id);
 
         return $qb->getQuery()->getArrayResult();
