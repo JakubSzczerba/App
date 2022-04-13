da78102d7508ec1c7dc02f3f2c9bb63f8647acc6
Jakub Szczerba <j.szczerba@yesindeed.pl>
2022-04-13 13:09:10 +0200

    Create edit acitons for filled proposals and generating new flag after edit need to change proposal + upgrade of user's proposals views

--- src/Controller/FeedbackController.php
+++ src/Controller/FeedbackController.php
@@ -4,23 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\Proposal;
 use App\Entity\ProposalFilled;
-use App\Form\FillProposalType;
-use App\Repository\ProposalRepository;
-use App\Repository\FilledProposalsRepository;
+use App\Form\EditFilledProposalType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class FeedbackController extends AbstractController
 {
     /**
      * @Route("/dashboard/myProposals/{id}", methods="GET|POST", name="viewProposal")
      */
-    public function viewProposal(EntityManagerInterface $entityManager, int $id)
+    public function viewProposal(int $id)
     {
         $viewProposal = $this->getDoctrine()->getRepository(ProposalFilled::class)->find(array('id' => $id));
         $new = 'Nowy';
@@ -31,6 +27,42 @@ class FeedbackController extends AbstractController
 
         return $this->render('User/viewFilledProposal.html.twig', [
             'proposal' => $viewProposal,
+        ]);
+    }
+
+    /**
+     * @Route("/dashboard/myProposals/edit/{id}", methods="GET|POST", name="editFilledProposal")
+     */
+    public function editFilledProposal(Request $request, EntityManagerInterface $entityManager, int $id)
+    {
+        $proposal = $this->getDoctrine()->getRepository(ProposalFilled::class)->find(array('id' => $id));
+        $new = 'Nowy';
+        $decision = '';
+        $needChange = 'Wymagana zmiana';
+        $corrected = 'Wymaga weryfiakcji';
+        $information = '';
+
+        $form = $this->createForm(EditFilledProposalType::class, $proposal);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($proposal->getDecision() == $needChange) {
+                $decision = $corrected;
+            } elseif ($proposal->getDecision() == $new) {
+                 $decision = $new;
+                }
+            $proposal->setDecision($decision);
+            $proposal->setInformation($information);
+
+            $entityManager->persist($proposal);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('myProposals');
+        }
+
+        return $this->render('User/editFilledProposalForm.html.twig', [
+            'form' => $form->createView(),
+            'proposal' => $proposal,
         ]);
     }
 
